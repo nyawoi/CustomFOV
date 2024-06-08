b8fcab26d172770b040a677997178624b60e40c0
nyawoi <106277673+nyawoi@users.noreply.github.com>
2024-06-08 14:54:50 -0500

    Rename variables to properly reflect object type

--- CustomFOV/Patches/PersistenceControllerPatch.cs
+++ CustomFOV/Patches/PersistenceControllerPatch.cs
@@ -14,8 +14,8 @@ public static class PersistenceControllerPatch
     public static void AddFOVSlider(PersistenceController __instance)
     {
         // Clone the FPS slider to create an FOV slider
-        var fpsSliderGameObject = __instance.graphicsMenu.gameObject.transform.Find("Menu/Body/Panel/SettingAnimation (1)");
-        var fovSliderGameObject = Object.Instantiate(fpsSliderGameObject, fpsSliderGameObject.parent);
+        var fpsSliderTransform = __instance.graphicsMenu.gameObject.transform.Find("Menu/Body/Panel/SettingAnimation (1)");
+        var fovSliderTransform = Object.Instantiate(fpsSliderTransform, fpsSliderTransform.parent);
 
         // Retrieve the FOV slider's label, logic component, and display
         // The current tree looks like so:
@@ -23,13 +23,13 @@ public static class PersistenceControllerPatch
         // - (1) Slider
         // - (2) LabelBox
         //   - (0) Label
-        var fovSliderLabel = fovSliderGameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
-        var fovSliderTranslation = fovSliderGameObject.transform.GetChild(0).gameObject.GetComponent<TranslatedTMPText>();
-        var fovSliderSlider = fovSliderGameObject.transform.GetChild(1).gameObject.GetComponent<Slider>();
-        var fovSliderDisplay = fovSliderGameObject.transform.GetChild(2).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        var fovSliderLabel = fovSliderTransform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        var fovSliderTranslation = fovSliderTransform.GetChild(0).gameObject.GetComponent<TranslatedTMPText>();
+        var fovSliderSlider = fovSliderTransform.GetChild(1).gameObject.GetComponent<Slider>();
+        var fovSliderDisplay = fovSliderTransform.GetChild(2).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
 
         // Change the GameObject's name for easier debugging using UnityExplorer
-        fovSliderGameObject.name = "SettingFieldOfView";
+        fovSliderTransform.name = "SettingFieldOfView";
         // Change the slider's label directly (translations will be done later)
         fovSliderTranslation.displayTag = "Field of View";
         fovSliderLabel.text = "Field of View";
